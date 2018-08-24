ee3cec5802fadba9e24c5400a1f435d6194f0983
Kannan Sudhakaran <kannan.mumbai@yahoo.com>
2018-08-24 11:36:53 +0530

    Update e-commerce.sql

--- node-app/express/ecommerce-app/e-commerce.sql
+++ node-app/express/ecommerce-app/e-commerce.sql
@@ -1,5 +1,5 @@
 create table category (product_category varchar(20),no_of_products int,primary key(product_category));
-create table products(product_guid int,name varchar(20),image varchar(20),description varchar(30),price int,product_category varchar(20),primary key(product_guid), foreign key (product_category) references category(product_category));
+create table products(product_guid int,name varchar(20),image varchar(20),description varchar(100),price int,product_category varchar(20),primary key(product_guid), foreign key (product_category) references category(product_category));
 create table customer(customer_guid int,username varchar(20),password varchar(20),registration_date varchar(20),primary key(customer_guid));
 create table product_order(order_id int,customer_guid int,no_of_items int,order_date varchar(20),primary key(order_id),foreign key(customer_guid) references customer(customer_guid));
 create table lineitems(lineitem_guid int,product_guid int,order_guid int,quantity int,primary key(lineitem_guid));
@@ -30,10 +30,11 @@ INSERT INTO `products` (`product_guid`, `name`, `image`, `description`, `price`,
 INSERT INTO `products` (`product_guid`, `name`, `image`, `description`, `price`, `product_category`) VALUES (8, 'High Sierra profound', 'hs.jpg', 'Ergonomically contoured, padded shoulder straps.', 2000, 'bags');
 INSERT INTO `products` (`product_guid`, `name`, `image`, `description`, `price`, `product_category`) VALUES (9, 'Mac Book pro', 'mac.jpg', 'Geared for exceptional performance with MacBook comes with Touch Bar with integrated Touch ID.', 90000, 'laptops');
 
+alter table organization add column id int primary key
 INSERT INTO `organization` (`id`, `name`, `address`, `contact`, `email`) VALUES (1, 'swabhav', 'andheri', 101, 'swabhav@swabhav.com');
 
 
 delete from cart;
 delete from lineitems;
-delete from product_order;
+delete from product_order;`organization`
 drop table cart;
